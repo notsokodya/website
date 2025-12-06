7af055f3ae23d5a6a94def5fd17d4785c893d58b
Kodya <notsokodya@gmail.com>
2025-12-06 23:50:05 +0300

    feat: add website handler (node)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
-    devToolbar: {
-        enabled: false
-    },
-    integrations: [react()]
+  devToolbar: {
+      enabled: false
+  },
+
+  integrations: [react()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
