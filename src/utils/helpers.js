c7ed85f69a2e7e50dacd7b3308636ef0dc027438
Kodya <notsokodya@gmail.com>
2025-12-20 19:43:38 +0300

    feat: add helper for commit hash
    me when `git rev-prase --short HEAD`

---
+++ src/utils/helpers.js
@@ -0,0 +1,17 @@
+let GIT_HASH
+let GIT_DATE
+export async function getWebsiteVersion() {
+    if (GIT_HASH !== undefined & GIT_DATE !== undefined) {
+        return {hash: GIT_HASH, date: GIT_DATE}
+    } else {
+        const childProcess = await import("node:child_process");
+
+        const hash = childProcess.execSync("git rev-parse --short HEAD").toString().trim();
+        const date = childProcess.execSync("git log -1 --format=%cd --date=short").toString().trim();
+
+        GIT_HASH = hash;
+        GIT_DATE = date;
+
+        return {hash, date}
+    }
+}
