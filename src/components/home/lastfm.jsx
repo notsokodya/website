d3d801d2347ae5940b76633745b0a9ffca0d52fb
Kodya <notsokodya@gmail.com>
2026-01-15 20:02:44 +0300

    feat: lastfm integration
    watch me listen random shit i find on yt music (or actual bangers, depends how i feel :p)

---
+++ src/components/home/lastfm.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+export default function LastFM({className, ...props}) {
+    const [tracks, setTracks] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchTrack = async () => {
+            setLoading(true);
+            setTracks([]);
+
+            const data = await fetch("/api/lastfm");
+            const tracksData = await data.json();
+
+            setTracks(tracksData || []);
+            setLoading(false);
+        }
+
+        fetchTrack();
+        setInterval(fetchTrack, 60000);
+    }, [])
+
+    return <>
+        <ul className={className} {...props}>
+            {tracks.map((track) => {
+                return <li>
+                    <a className="track" href={track.url} {...props}>
+                        <img width="160px" height="160px" src={track.cover} {...props}/>
+                        <strong className="title" {...props}>{track.name}</strong>
+                        <span className="artist" {...props}>{track.artist}</span>
+                        <span className="icons" {...props}>
+                            {track.isPlaying ? <span className="playing" title="Now Playing" {...props}>▶</span> : ""}
+                            {track.loved ? <span className="loved" title="Loved" {...props}>❤</span> : ""}
+                        </span>
+                    </a>
+                </li>
+            })}
+        </ul>
+    </>
+}
