33c20214fc655ac0315dacedc3fa244d2b624f23
Kodya <notsokodya@gmail.com>
2026-01-16 01:18:40 +0300

    fix: astro making my api static
    WHY YOU ARE COMPILING API ENDPOINTS INTO STATIC API WHYYYYYYYYYYYYYYYYYYYYYY

--- src/pages/api/lastfm.js
+++ src/pages/api/lastfm.js
@@ -1,6 +1,12 @@
 import { tracks } from "@/utils/lastfm";
 
-export function GET() {
+export const prerender = false;
+
+export const GET = async () => {
+    if (!tracks) {
+        return new Response(null, {status: 503})
+    }
+
     return new Response(JSON.stringify(tracks), {
         status: 200,
         headers: {
